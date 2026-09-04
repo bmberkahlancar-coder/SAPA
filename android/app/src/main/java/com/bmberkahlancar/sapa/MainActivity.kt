f07988793d3413cced48f179d7614cf86847d562
bmberkahlancar-coder <bm.berkahlancar@gmail.com>
2026-09-04 12:11:46 +0700

    Update MainActivity.kt

--- android/app/src/main/java/com/bmberkahlancar/sapa/MainActivity.kt
+++ android/app/src/main/java/com/bmberkahlancar/sapa/MainActivity.kt
@@ -1,19 +1,5 @@
 package com.bmberkahlancar.sapa
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val textView = TextView(this)
-        textView.text = "SAPA"
-        textView.textSize = 32f
-        textView.setPadding(40, 80, 40, 40)
-
-        setContentView(textView)
-    }
-}
+class MainActivity : FlutterActivity()
